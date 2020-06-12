bd3f74dea9346a62d35732a9e2b558c48af56546
tyler9937 <tyler9937@users.noreply.github.com>
2020-06-12 13:59:38 -0400

    finished sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,9 +1,11 @@
 from room import Room
 from player import Player
 from world import World
+import csv
 
 import random
 from ast import literal_eval
+
 
 # Load world
 world = World()
@@ -27,7 +29,93 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+
+
+switch = True
+seed_cache = {}
+path_length_cache = {}
+seed_var = 90870
+
+# Taking advantage of random path selection to shrink path lenght
+while switch is True:
+
+    if seed_var in seed_cache:
+        seed_var += 1
+
+    # Initating mutable variables
+    player = Player(world.starting_room)
+    
+    
+    traversal_string = ''
+    traversal_path = []
+    reverese_directions = {'n':'s', 's':'n','e':'w','w':'e'} # Oppistee direction referernce for travaling backwards
+    reverse_path = []
+    visited = {}
+
+    # Initaiting starting room in visited
+    visited[player.current_room.id] = player.current_room.get_exits()
+
+    # Looping till all rooms visited
+    while len(visited) < len(room_graph) -1:
+
+        # Adding room to visited
+        if player.current_room.id not in visited:
+            
+            visited[player.current_room.id] = player.current_room.get_exits()
+            
+            # Remove room that was entered from
+            visited[player.current_room.id].remove(reverse_path[-1])
+
+
+        # If in a dead end
+        while len(visited[player.current_room.id]) == 0:
+
+            # Start backtracking
+            reverse = reverse_path.pop()
+            traversal_path.append(reverse) 
+            player.travel(reverse)
+
+
+        # Adding Randomness to path selection
+        choices = visited[player.current_room.id]
+        choice = random.choice(choices)
+        index = choices.index(choice)
+        movement = visited[player.current_room.id].pop(index) # Movement equal to random direction
+
+
+        # Update paths and move player object
+        traversal_path.append(movement)
+        reverse_path.append(reverese_directions[movement])
+        player.travel(movement)
+
+
+    path_length = len(traversal_path)
+    seed_cache[seed_var] = path_length
+    
+
+    # TRAVERSAL TEST
+    if path_length < 990 and path_length not in path_length_cache:
+        print(str(seed_var) +' '+ str(path_length))
+    if path_length < 990:
+        print(seed_var)
+        visited_rooms = set()
+        player.current_room = world.starting_room
+        visited_rooms.add(player.current_room)
+
+        for move in traversal_path:
+            player.travel(move)
+            visited_rooms.add(player.current_room)
+
+        if len(visited_rooms) == len(room_graph):
+                switch = False
+                
+                
+ 
+
+    path_length_cache[path_length] = seed_var
+
+
+
 
 
 
@@ -42,6 +130,7 @@ for move in traversal_path:
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
@@ -51,12 +140,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,17 +1,25 @@
+import random
+from collections import deque
+
 class User:
     def __init__(self, name):
         self.name = name
 
 class SocialGraph:
+
+
     def __init__(self):
         self.last_id = 0
         self.users = {}
         self.friendships = {}
+        self.count = 0
 
     def add_friendship(self, user_id, friend_id):
         """
         Creates a bi-directional friendship
         """
+        self.count += 1
+
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
@@ -19,7 +27,7 @@ class SocialGraph:
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
-
+            
     def add_user(self, name):
         """
         Create a new user with a sequential integer ID
@@ -28,25 +36,55 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.friendships[vertex_id]
+
     def populate_graph(self, num_users, avg_friendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
-
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
-        # Add users
+        for i in range(0, num_users):
+            self.add_user(f'User {i}')
 
-        # Create friendships
+        possible_friendships = []
+
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+
+
+    def bfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
+        q = deque()
+        q.append([starting_vertex])
+
+        visited = set()
+
+        while q:
+            path = q.popleft()
+            v = path[-1]
+            if v not in visited:
+                if v == destination_vertex:
+                    return path
+                visited.add(v)
+                for n in self.get_neighbors(v):
+                    q.append([*path, n])
+        return None
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,8 +97,26 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        
+        for user in self.users:
+            path = self.bfs(user_id, user)
+            visited[user] = path
+
         return visited
 
+'''
+ 3. Question
+    1. To create 100 users with an average of 10 friends each, how many times would you need to call add_friendship()? Why?
+    
+        - In this case it would take 500 function calls. In any case it would be the number of users * the desired average // 2.
+        This is because: Firstly, the divided by 2 part just accounts for the fact that when when user makes a friendship its actully
+        new friendships occuring. So lets drop the // 2 for understanding. We have 100 users and we want an avaarge of 10 friends per user.
+        that means we would have to create 1000 friendships from a randomly distributied source so that so that each user has chance of
+        gaining 10 friends (1000frindships / 100 users = 10friends / user). Since its random some people will have more than 10 and some
+        people will have less but the average will work out to be 10.
+
+    2. near 100% average 500 degrees of seperation
+'''
 
 if __name__ == '__main__':
     sg = SocialGraph()
@@ -68,3 +124,4 @@ if __name__ == '__main__':
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
+    print(sg.count)
